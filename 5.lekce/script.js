8f2cfa9b1f8369d85268fa507ba9d4dbf5879b41
klaranemeckova6 <1273@GMHS.cz>
2026-03-13 10:18:41 +0100

    27.2, 6.3. (4.,5. lekce)

---
+++ 5.lekce/script.js
@@ -0,0 +1,32 @@
+let isLoggednIn = true;
+let isAdmin = false;
+ 
+if (isLoggednIn) {
+    console.log("Vítej na stránce");
+ 
+    if(isAdmin) {
+        console.log("Přístup do adminu povolen.")
+    } else {
+        console.log("Jsi přihlášen jako uživatel!")
+    }
+} else {
+    console.log("Nejsi přihlášený")
+}
+ 
+let age = 17;
+let hasDriverLicense = true;
+ 
+if (age >= 18 && hasDriverLicense) {
+    console.log("Může řídit!")
+} else {
+    console.log("Nemůžeš řídit!")
+}
+ 
+let isHoliday = false;
+let isWeekend = true;
+ 
+if (isHoliday || isWeekend) {
+    console.log("Je čas relaxovat");
+    } else {
+        console.log("Nemusíme pracovat!")
+    }
