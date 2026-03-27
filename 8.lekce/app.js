a051d6389cbf66d684fd60309c8bb1ff7b7d8c75
klaranemeckova6 <1273@GMHS.cz>
2026-03-27 10:35:13 +0100

    27.3.

--- 8.lekce/app.js
+++ 8.lekce/app.js
@@ -1,6 +1,30 @@
-let counter = 0;
+let counter = 2;
 
 while (counter < 10) {
-    console.log(counter)
+    let message = counter
+    if (counter === 7) {
+       message = "7 je tady";
+    }
+    console.log(message)
     counter++;
+}
+
+for (let i = 0; i < 10; i++) {
+    if(i === 5) continue;
+    console.log(i)
+}
+
+for (let i = 2; i <= 100; i++) {
+  if (i % 2 === 0 && i !== 50) {
+    console.log(i);
+  }
+}
+
+for (let i = 2; i <= 100; i++) {
+  if (i === 50) {
+    continue;
+  }
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
