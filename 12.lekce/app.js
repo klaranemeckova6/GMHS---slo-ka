308764ac53a1921a6a1974342aded2f2a8a2a98f
klaranemeckova6 <1273@GMHS.cz>
2026-05-22 10:35:45 +0200

    22.05.2026

---
+++ 12.lekce/app.js
@@ -0,0 +1,13 @@
+//Deklarace
+//Cyklus for let i = (parametr); i <= (parametr); i++
+//console.log(i;)
+
+//Volání funkce
+
+printRange(1,4)
+
+function printRange(start, end) {
+  for (let i = start; i <= end; i++) {
+    console.log(i);
+  }
+}
